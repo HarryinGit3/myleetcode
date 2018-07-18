ff6122dab3b1112955031cea22074d701537556b
e <yh821219454@163.com>
2018-07-18 22:32:03 +0800

    排序与leetcode 2,79

---
+++ leetcode/常规算法/排序/插入排序.py
@@ -0,0 +1,11 @@
+lists=[12,44,23,47,87,13,34]
+'''插入排序—————直接插入排序(Straight Insertion Sort)''' 
+'''
+基本思想：
+将一个要素插入到有序表中，形成新的有序表
+'''
+# 插入排序函数
+def insert_sort(lists):
+    count=len(lists)
+    
+ 
